54db99fabca3ca24128c46dfa111e7717825c07f
alrazamc <alrazamc@gmail.com>
2018-06-06 03:50:05 +0500

    added daily worker procfile

--- application/controllers/Worker.php
+++ application/controllers/Worker.php
@@ -9,8 +9,8 @@ class Worker extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        // if(!is_cli()) 
-        //     exit(0);
+        if(!is_cli()) 
+            exit(0);
     }
 
     //Delete S3 media of category and delete content after that
@@ -129,7 +129,7 @@ class Worker extends CI_Controller {
     {
         $this->load->library('queue');
         $this->queue->rebuild_random($user_id);
-        echo "User-$user_id queue reconstructed ";
+        echo "User-$user_id Random queue reconstructed ";
     }
 
     //child cron, publish single batch of around 50 posts

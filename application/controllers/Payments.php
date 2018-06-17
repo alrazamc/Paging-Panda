3b3025cc16bfe4b144b8b3b476c96c84161025f4
alrazamc <alrazamc@gmail.com>
2018-06-17 19:11:22 +0500

    test

--- application/controllers/Payments.php
+++ application/controllers/Payments.php
@@ -95,11 +95,13 @@ class Payments extends CI_Controller {
         {
             $previous_plan = $this->payments_model->get_plan($user->plan_id);
             $last_invoice = $this->payments_model->get_user_last_invoice($user->user_id);
+        }
+        $this->payments_model->approve_payment($user, $plan, $trial_days_remaining);
+        if($user->on_trial == NO && $user->status == USER_STATUS_ACTIVE)
+        {
             $refund_msg = $this->_refund($previous_plan, $last_invoice, $user, $plan); //refund remaining amount of last plan
             $this->_stop_recurring($last_invoice, RECUR_STOP_REASON_CHANGE_OF_PLAN); //stop last recurring plan
-        }//active user changing plan
-
-        $this->payments_model->approve_payment($user, $plan, $trial_days_remaining);
+        }
         $this->session->set_flashdata('alert', get_alert_html("$plan->name plan activated successfully. $refund_msg Thank you for choosing ".getenv("SITE_NAME"), ALERT_TYPE_SUCCESS));
         redirect('users/settings');
     }

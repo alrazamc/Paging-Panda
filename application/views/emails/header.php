d52309b168c367cef4c650144ad22e2d0b1dc801
alrazamc <alrazamc@gmail.com>
2018-06-20 16:50:32 +0500

    emails 0.3

--- application/views/emails/header.php
+++ application/views/emails/header.php
@@ -12,7 +12,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php echo getenv('SITE_NAME') ?></title>
+        <title></title>
         <style type="text/css">
             p{
             margin:10px 0;
@@ -465,7 +465,7 @@
     </head>
     <body>
         <!--*|IF:MC_PREVIEW_TEXT|*-->
-        <!--[if !gte mso 9]>--><span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;"><?php echo getenv('SITE_NAME') ?></span><!--<![endif]-->
+        <!--[if !gte mso 9]>--><span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;"></span><!--<![endif]-->
         <!--*|END:IF|*-->
         <center>
             <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">

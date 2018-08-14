36e4e0754a4bd28bdb8e3302a399f594a5105f7c
alrazamc <alrazamc@gmail.com>
2018-08-14 10:01:23 +0500

    FB pixed signup event

--- application/views/footer.php
+++ application/views/footer.php
@@ -30,26 +30,6 @@
 		  gtag('js', new Date());
 		  gtag('config', 'UA-55986435-3');
 		</script>
-		<?php if($this->session->flashdata('signup')){ ?>
-		<!-- Facebook Pixel Code -->
-		<script>
-		  !function(f,b,e,v,n,t,s)
-		  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-		  n.queue=[];t=b.createElement(e);t.async=!0;
-		  t.src=v;s=b.getElementsByTagName(e)[0];
-		  s.parentNode.insertBefore(t,s)}(window, document,'script',
-		  'https://connect.facebook.net/en_US/fbevents.js');
-		  fbq('init', '237126063803110');
-		  fbq('track', 'PageView');
-		</script>
-		<noscript><img height="1" width="1" style="display:none"
-		  src="https://www.facebook.com/tr?id=237126063803110&ev=PageView&noscript=1"
-		/></noscript>
-		<!-- End Facebook Pixel Code -->
-		<?php } ?>
-
 	<?php } ?>
 </body>
 </html>
